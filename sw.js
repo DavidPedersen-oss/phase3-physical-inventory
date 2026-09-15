c65ab507a361c0bb3505d62f1480a5d992bc74ee
David Pedersen <david.pedersen@csulb.edu>
2026-09-15 15:09:26 -0700

    Accept alternate inventory workbook headers

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'phase3-shell-v8';
+const CACHE = 'phase3-shell-v9';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
